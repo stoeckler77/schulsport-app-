cb40b842b86e320741108a7c5b975eff5586bb42
stefan <stefan@stefans-MBP.fritz.box>
2025-02-28 09:26:50 +0100

    Add Schule Muri logo to header

---
+++ react-client/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Make sure to create this CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img 
+            src="/images/schule-muri-logo.png" 
+            alt="Schule Muri Logo" 
+            height="50" 
+            className="me-2"
+          />
+          <span>Schulsport App</span>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/courses">Kurse</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar; 
