7e3ff8db4403cf67baa83ef379341a67ef258377
stefan <stefan@stefans-MBP.fritz.box>
2025-02-28 11:44:21 +0100

    Replace logo with inline SVG version

--- react-client/src/components/Navbar.js
+++ react-client/src/components/Navbar.js
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Make sure to create this CSS file
 // Import the logo directly from assets
 // import schuleMuriLogo from '../assets/schule-muri-logo.png';
 
 const Navbar = () => {
-  useEffect(() => {
-    console.log('Logo path:', schuleMuriLogo);
-  }, []);
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand d-flex align-items-center" to="/">
+          {/* Inline SVG logo based on the red Schule Muri logo */}
           <div className="logo-container me-2">
-            <img 
-              src="https://www.schule-muri.ch/custom/images/logo.png" 
-              alt="Schule Muri Logo" 
-              className="logo-image"
-            />
+            <svg 
+              width="150" 
+              height="40" 
+              viewBox="0 0 400 100" 
+              xmlns="http://www.w3.org/2000/svg"
+              className="logo-svg"
+            >
+              <rect width="400" height="100" fill="#c1121f" />
+              <path d="M50,20 v40 h20 v-20 h20 v-20 h-40" fill="white" />
+              <path d="M90,20 v40 h20 v-20 h20 v-20 h-40" fill="white" />
+              <path d="M130,20 v40 h20 v-20 h20 v-20 h-40" fill="white" />
+              <path d="M170,20 h180 v60 h-350 v-20 h170 v-40" fill="white" />
+              <text x="200" y="40" font-family="Arial" font-size="16" fill="white" text-anchor="middle">SCHULE</text>
+              <text x="240" y="70" font-family="Arial" font-size="24" font-weight="bold" fill="#c1121f" text-anchor="middle">MURI</text>
+            </svg>
           </div>
           <span>Schulsport App</span>
         </Link>
